fbe848e08c228944efe4342b1a60451d66d4f13b
Simon Thelin <simon.thelin90@gmail.com>
2017-03-12 12:51:42 +0100

    library problem?

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App width={400} height={330}/>,
+  document.querySelector('#root')
+);
